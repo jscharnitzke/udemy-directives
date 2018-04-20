ae3544ce5a39cc67dfb7d5dabf1902085796125b
Joe Scharnitzke <j.scharnitzke@gmail.com>
2018-04-19 18:53:33 -0700

    Catching up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,9 +9,18 @@ export class AppComponent {
   title = 'app';
   serverName = 'testserver';
   serverCreated = false;
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created';
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   createServer() {
     this.serverCreated = true;
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
 
 }
